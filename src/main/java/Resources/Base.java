058ef277d247763825f04031a5886bb8dc9c5b14
Luigi-Cirillo <caselladiposta@yahoo.com>
2020-01-03 13:17:00 +0100

    Added GIT test 2 Base.class

--- src/main/java/Resources/Base.java
+++ src/main/java/Resources/Base.java
@@ -40,6 +40,7 @@ public class Base {
 		file.close();
 		//String browserName=prop.getProperty("browser");
 		System.out.println("GIT test");
+		System.out.println("GIT test in develop branch");
 		//mvn test -Dbrowser=chrome writes in System.properties the browser name
 		String browserName=System.getProperty("browser");
 		String isHeadless=System.getProperty("headless");
